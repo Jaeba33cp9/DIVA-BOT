d803c3a6da4185f1a0e9a841ad5549fcfe19be15
Jaeba <example@gmail.com>
2026-05-22 02:53:43 +0300

    debug channel

--- index.js
+++ index.js
@@ -9,21 +9,31 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-// حط ID ديال التاب هنا
-const CHANNEL_ID = '1501394092884492378';
+// حط Channel ID هنا
+const CHANNEL_ID = '1498049200334700584';
 
-// ملي البوت يشعل
 client.once('clientReady', async () => {
   console.log(`${client.user.tag} online`);
 
   try {
+
     const channel = await client.channels.fetch(CHANNEL_ID);
 
-    if (channel) {
-      channel.send('DIVA BOT ONLINE 🔥');
+    console.log('CHANNEL:', channel);
+
+    if (!channel) {
+      console.log('CHANNEL NOT FOUND');
+      return;
     }
-  } catch (err) {
-    console.log('Channel Error:', err);
+
+    await channel.send('DIVA BOT ONLINE 🔥');
+
+    console.log('MESSAGE SENT');
+
+  } catch (error) {
+
+    console.log('CHANNEL ERROR:', error);
+
   }
 });
 
@@ -42,11 +52,11 @@ client.on('shardResume', () => {
 
 // anti crash
 process.on('unhandledRejection', error => {
-  console.log('Unhandled promise rejection:', error);
+  console.log('Unhandled Rejection:', error);
 });
 
 process.on('uncaughtException', error => {
-  console.log('Uncaught exception:', error);
+  console.log('Uncaught Exception:', error);
 });
 
 client.login(process.env.TOKEN);
