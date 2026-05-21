3f855936f8c96b658d7905bcbf3c8c5c7adc7a4b
Jaeba <example@gmail.com>
2026-05-22 02:37:03 +0300

    test token

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+console.log("TOKEN:", process.env.TOKEN);
 
-console.log(process.env.TOKEN);
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
