961ba955e330bb64209388a7617359b07cd8c9fc
Jaeba <example@gmail.com>
2026-05-22 02:46:26 +0300

    update bot

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 require('dotenv').config();
 
-console.log("TOKEN:", process.env.TOKEN);
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const {
+  Client,
+  GatewayIntentBits
+} = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('clientReady', () => {
+// حط ID ديال التاب هنا
+const CHANNEL_ID = '1501394092884492378';
+
+// ملي البوت يشعل
+client.once('clientReady', async () => {
   console.log(`${client.user.tag} online`);
+
+  try {
+    const channel = await client.channels.fetch(CHANNEL_ID);
+
+    if (channel) {
+      channel.send('DIVA BOT ONLINE 🔥');
+    }
+  } catch (err) {
+    console.log('Channel Error:', err);
+  }
+});
+
+// reconnect logs
+client.on('shardDisconnect', () => {
+  console.log('Bot disconnected!');
+});
+
+client.on('shardReconnecting', () => {
+  console.log('Bot reconnecting...');
+});
+
+client.on('shardResume', () => {
+  console.log('Bot resumed connection!');
+});
+
+// anti crash
+process.on('unhandledRejection', error => {
+  console.log('Unhandled promise rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+  console.log('Uncaught exception:', error);
 });
 
 client.login(process.env.TOKEN);
